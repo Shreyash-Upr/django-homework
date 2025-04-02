6bb6fba80d487de13388d93e369bf9b55972e1e7
Shreyash-Upr <023NEB927@sxc.edu.np>
2025-04-02 20:20:45 +0545

    Made changes

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from teacher.models import Teacher, Course, Department
 
 # Register your models here.
+
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Department)
+
